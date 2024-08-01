20854019af40d1ba4467b88079cac52de7e4c18a
Abdullah Salih Öner <riyadlioner00@gmail.com>
2024-08-01 16:56:33 +0300

    Initial commit

---
+++ src/main/java/com/salih/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package com.salih.repository;
+
+import com.salih.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+  List<Tutorial> findByPublished(boolean published);
+
+  List<Tutorial> findByTitleContaining(String title);
+}
